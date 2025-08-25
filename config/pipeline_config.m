ff64d3f7a25a68add5e4e65af3081446e611e7ab
James P. Herman <36748290+jamespherman@users.noreply.github.com>
2025-08-25 08:54:34 -0400

    Project root vs. OneDrive root
    Directory definitions were changed to be relative to the OneDrive root rather than the project root.

--- config/pipeline_config.m
+++ config/pipeline_config.m
@@ -4,13 +4,18 @@ function config = pipeline_config()
     %   Returns:
     %       config (struct): A struct containing all configuration parameters.
 
-    % Get the project root directory
-    projectRoot = utils.find_project_root();
+    % Get the OneDrive 'root' directory in which we expect to find the raw
+    % neuronal data directory, the nested behavioral output from PLDAPS
+    % directory, and the place we'll create a directory to store the .DAT
+    % file and other Kilosort output for each session's data.
+    oneDriveRoot = utils.findOneDrive();
 
     % Define directory paths
-    config.rawNeuralDataDir = fullfile(projectRoot, 'Neuronal Data');
-    config.behavioralDataDir = fullfile(projectRoot, 'Behavioral Data', 'PLDAPS_output', 'output');
-    config.analysisOutputDir = fullfile(projectRoot, 'Neuronal Data Analysis');
+    config.rawNeuralDataDir = fullfile(oneDriveRoot, 'Neuronal Data');
+    config.behavioralDataDir = fullfile(oneDriveRoot, 'Behavioral Data', ...
+        'PLDAPS_output', 'output');
+    config.analysisOutputDir = fullfile(oneDriveRoot, ...
+        'Neuronal Data Analysis');
 
     % Define other parameters
     config.samplingRate = 30000; % Sampling rate in Hz
